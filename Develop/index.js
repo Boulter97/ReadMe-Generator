66e0aa050434b056e462899413c977a3a76f6edc
Boulter97 <118101244+Boulter97@users.noreply.github.com>
2023-07-09 21:35:50 +1000

    added questions to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,48 @@ const fs = require ('fs');
 const inquirer = require('requirer');
 const generateMarkdown = require ('./generateMarkdown');
 
-const questions = [];
+const questions = [
+    { type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+
+    { type: 'input',
+      name: 'title',
+      message: 'What is your decription for your project?',
+    },
+
+    { type: 'input',
+      name: 'title',
+      message: 'input your table of contents',
+    },
+    { type: 'input',
+      name: 'title',
+      message: 'how do you install/run your project?',
+    },
+    { type: 'input',
+      name: 'title',
+    message: 'What is the use of this project?',
+    },
+    { type: 'input',
+      name: 'title',
+      message: 'Choose a licence for your project',
+    },
+    { type: 'input',
+      name: 'title',
+      message: 'Who has contributed to this project?',
+    },
+    { type: 'input',
+      name: 'title',
+      message: 'how do you test your project?',
+    },
+
+    { type: 'input',
+      name: 'title',
+      message: 'common questions about your project?',
+    },
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
